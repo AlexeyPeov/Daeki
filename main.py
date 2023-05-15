fa262c35ca2a8875aea46619643cd754b847f91b
RealEragon <92983632+RealEragon@users.noreply.github.com>
2023-05-15 20:50:23 +0300

    Этот код работает уже с API сервиса tmdb, спрашивает диапазон рейтинга и выдаёт случайные фильмы с их рейтингом, названием и ссылкой на постер из заданного диапазона

--- main.py
+++ main.py
@@ -1,92 +1,83 @@
-import random
-import textwrap
-
 import requests
-from bs4 import BeautifulSoup
+import json
+import random
+import os
 
-while True:
-    min_rating = float(input("Введите минимальный рейтинг (от 0 до 10): "))
-    if min_rating < 0 or min_rating > 10:
-        print("Пожалуйста, введите число от 0 до 10.")
+
+def get_movie(min_rating, max_rating):
+    # Получаем случайный фильм из TMDB с рейтингом в заданном диапазоне
+    url = f"https://api.themoviedb.org/3/discover/movie?api_key=fb3b3e4a8c47e10407fa8a54e2010d5e&language=ru-RU&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_average.gte={min_rating}&vote_average.lte={max_rating}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    if data['total_results'] == 0:
+        return None
     else:
-        break
-
-url = "https://www.imdb.com/search/title/?title_type=feature,tv_movie,tv_series&user_rating=" + str(min_rating) + "," + str(min_rating+0.1)
-
-
-response = requests.get(url)
-
-soup_first = BeautifulSoup(response.text, "html.parser")
-desc_div = soup_first.select_one(".desc span:nth-of-type(1)")
-available_movies_text = desc_div.text
-available_movies = int(available_movies_text.split()[-2].replace(',', ''))
-print(available_movies)
-
-url_precise = "https://www.imdb.com/search/title/?title_type=feature,tv_movie,tv_series&user_rating=" \
-        + str(min_rating) + "," + str(min_rating+0.1) \
-        + "&start=" + str((random.randint(1, available_movies // 50) * random.randint(1, 50)) + 1) + "&ref_=adv_nxt"
-
-response_precise = requests.get(url_precise)
-
-soup = BeautifulSoup(response_precise.text, "html.parser")
-
-movies = soup.select(".lister-item-header a")
-ratings = soup.select(".ratings-imdb-rating")
-
-years = soup.select(".lister-item-year.text-muted.unbold")
-descriptions = soup.select(".lister-item-content p:nth-of-type(2)")
+        results = data['results']
+        random_movie = random.choice(results)
+        movie_id = random_movie['id']
+        movie_title = random_movie['title']
+        movie_overview = random_movie['overview']
+        movie_rating = random_movie['vote_average']
+        poster_path = random_movie['poster_path']
+        return (movie_id, movie_title, movie_overview, movie_rating, poster_path)
 
 
-movies_ratings = []
-for movie, rating, year, description in zip(movies, ratings, years, descriptions):
-    title = movie.text
-    rating_value = float(rating.strong.text)
-    year_value = year.text.strip("()")
-    description_value = description.text.strip()
-
-    if not description_value or description_value == "Add a Plot":
-        description_value = "No description"
-
-    movies_ratings.append((title, rating_value, year_value, description_value))
-
-random_entry = random.choice(movies_ratings)
-movie, rating, year, description = random_entry
-
-wrapped_description = textwrap.wrap(description, width=50)
-
-with open("movies.txt", "r", encoding="utf-8") as f:
-    printed_movies = [line.strip().split(",")[0] for line in f]
-    if movie not in printed_movies:
-        print(f"{movie} ({year}), {rating}")
-        for line in wrapped_description:
-            print(line)
-        with open("movies.txt", "a", encoding="utf-8") as file:
-            file.write(f"{movie}, {year}\n")
+def write_to_file(movie_id):
+    # Записываем ID фильма в файл исключений
+    with open("exceptions.txt", "a") as file:
+        file.write(str(movie_id) + "\n")
 
 
-    # while True:
-    #     for i in range(len(movies)):
-    #         with open('movies.txt', 'r', encoding='utf-8', errors='ignore') as f:
-    #             if movies[i].text in f.read():
-    #                 continue
-    #         print(movies[i].text + " - " + ratings[i].text.strip())
-    #         while True:
-    #             choice = input("Вам нравится фильм? (да/нет): ")
-    #             if choice.lower() == 'да' or choice.lower() == 'нет':
-    #                 break
-    #             else:
-    #                 print("Пожалуйста, введите 'да' или 'нет'.")
-    #         if choice.lower() == 'да':
-    #             print("Отлично! Наслаждайтесь просмотром!")
-    #             break
-    #         else:
-    #             file.write(movies[i].text + " - " + ratings[i].text.strip() + "\n")
-    #     if choice.lower() == 'да':
-    #         break
-    #     next_url = soup.select(".desc a")[-1]['href']
-    #     next_response = requests.get("https://www.imdb.com" + next_url)
-    #     soup = BeautifulSoup(next_response.text, "html.parser")
-    #     movies = soup.select(".lister-item-header a")
-    #     ratings = soup.select(".ratings-imdb-rating")
-    #     if not movies:
-    #         break
+def read_from_file():
+    # Читаем ID фильмов из файла исключений
+    with open("exceptions.txt", "r") as file:
+        lines = file.readlines()
+        exceptions = [int(line.strip()) for line in lines]
+        return exceptions
+
+
+def main():
+    # Создаем файл исключений, если его еще нет
+    if not os.path.exists("exceptions.txt"):
+        open("exceptions.txt", "w").close()
+
+    # Получаем минимальный и максимальный рейтинг от пользователя
+    min_rating = float(input("Введите минимальный рейтинг: "))
+    max_rating = float(input("Введите максимальный рейтинг: "))
+
+    # Получаем список ID фильмов из файла исключений
+    exceptions = read_from_file()
+
+    while True:
+        # Получаем случайный фильм из TMDB с рейтингом в заданном диапазоне
+        movie_data = get_movie(min_rating, max_rating)
+
+        if movie_data is None:
+            print("Нет фильмов в заданном диапазоне.")
+            break
+
+        movie_id, movie_title, movie_overview, movie_rating, poster_path = movie_data
+
+        if movie_id in exceptions:
+            continue
+
+        print(f"Название: {movie_title}")
+        print(f"Описание: {movie_overview}")
+        print(f"Рейтинг: {movie_rating}")
+
+        # Выводим постер фильма
+        base_url = "https://image.tmdb.org/t/p/w500"
+        poster_url = base_url + poster_path if poster_path else "https://via.placeholder.com/500x750.png?text=No+poster+available"
+        print(f"Постер: {poster_url}")
+
+        # Спрашиваем пользователя, хочет ли он посмотреть этот фильм
+        answer = input("Хотите посмотреть этот фильм? (y/n) ")
+
+        if answer.lower() == "y":
+            break
+        else:
+            write_to_file(movie_id)
+
+
+if __name__ == "__main__":
+    main()
